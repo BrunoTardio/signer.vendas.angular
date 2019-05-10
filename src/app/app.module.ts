de1a999694250e5bdfa75420476c14d543ca2f34
Bruno Tardio <brunotardiosi@gmail.com>
2019-05-10 19:08:08 -0300

    Projeto Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/Material';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {LoginModule, LoginRoutingModule} from './autenticacao';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,        
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LoginModule,
+    LoginRoutingModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
